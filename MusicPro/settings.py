2ead0d79c24479214c666ec9ee000d921ad0b816
Cocoke <jorge.a.palma.c@gmail.com>
2022-05-22 22:50:12 -0400

    uwu7

--- MusicPro/settings.py
+++ MusicPro/settings.py
@@ -128,6 +128,7 @@ import os
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
