5aa86d46e81502d54200ff9d795a03b5af21408b
Cocoke <jorge.a.palma.c@gmail.com>
2022-05-23 22:18:47 -0400

    uwu88

--- MusicPro/settings.py
+++ MusicPro/settings.py
@@ -124,7 +124,6 @@ USE_TZ = True
 import os
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'djangohost', 'static')
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
